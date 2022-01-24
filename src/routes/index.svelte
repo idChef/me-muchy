fb5efe65b351a68c6938e51b7d268cd526519ede
Tymoteusz Kossek <tymoteusz.kossek@dreamstormstudios.com>
2022-01-24 15:46:03 +0100

    Added basic post fetch

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,22 @@
 <script>
-import Meme from "$lib/components/Meme/Meme.svelte";
+	import Meme from '$lib/components/Meme/Meme.svelte';
 
+	import { onMount } from 'svelte';
+
+	let memes;
+
+	onMount(async () => {
+		const res = await fetch(`https://memuchyapi.azurewebsites.net/Post/GetAll`);
+		memes = await res.json();
+	});
 </script>
 
 <svelte:head>
-    <title>Me-muchy - Memy bez cenzury</title>
+	<title>Me-muchy - Memy bez cenzury</title>
 </svelte:head>
 
-<Meme/>
-<Meme/>
-<Meme/>
-<Meme/>
+{#if memes}
+{#each memes as meme}
+<Meme image={meme.picture} title={meme.title} />
+{/each}
+{/if}
