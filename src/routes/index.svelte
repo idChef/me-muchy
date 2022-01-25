1b935cd4bce6ab67df506f4b6021e742ab45390f
Tymoteusz Kossek <tymoteusz.kossek@dreamstormstudios.com>
2022-01-25 20:29:32 +0100

    Added infinite scroll for main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,29 @@
-<script>
+<script lang="ts">
 	import Meme from '$lib/components/Meme/Meme.svelte';
+	import InfiniteScroll from 'svelte-infinite-scroll';
 
-	import { onMount } from 'svelte';
+	let page = 0;
+	let pageSize = 5;
+	let hasMore = true;
+	
+	let memes = [];
+	$: fetchNextPage(page);
 
-	let memes;
+	const fetchNextPage = async (page) => {
+		if (!hasMore) return;
 
-	onMount(async () => {
-		const res = await fetch(`https://memuchyapi.azurewebsites.net/Post/GetAll?pageSize=100&pageindex=0`);
-		memes = await res.json();
-	});
+		const res = await fetch(
+			`https://memuchyapi.azurewebsites.net/Post/GetAll?pageSize=${pageSize}&pageindex=${page}`
+		);
+		const fetchedMemes = [...(await res.json())];
+
+		if (fetchedMemes.length < 1) {
+			hasMore = false;
+			return;
+		}
+
+		memes = [...memes, ...fetchedMemes];
+	};
 </script>
 
 <svelte:head>
@@ -16,15 +31,22 @@
 </svelte:head>
 
 <div class="flex flex-col gap-14 pt-14">
-{#if memes}
-{#each memes as meme}
-<Meme image={meme.picture} title={meme.title} postId={meme.id} upvotes={meme.n_like} downvotes={meme.n_unlike} />
-{/each}
-{:else}
-<Meme isLoading={true} />
-<Meme isLoading={true} />
-<Meme isLoading={true} />
-<Meme isLoading={true} />
-<Meme isLoading={true} />
-{/if}
+	{#if memes}
+		{#each memes as meme}
+			<Meme
+				image={meme.picture}
+				title={meme.title}
+				postId={meme.id}
+				upvotes={meme.n_like}
+				downvotes={meme.n_unlike}
+			/>
+		{/each}
+	{:else}
+		<Meme isLoading={true} />
+		<Meme isLoading={true} />
+		<Meme isLoading={true} />
+		<Meme isLoading={true} />
+		<Meme isLoading={true} />
+	{/if}
 </div>
+<InfiniteScroll threshold={100} on:loadMore={() => page++} window />
